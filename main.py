71d45b108771facc89703c2f43abf0fd021e7b83
Jorge Alexandre Teixeira Henriques Luis <86257696+JorgeLuis1113@users.noreply.github.com>
2025-03-28 16:38:49 -0300

    Update main.py
    Após ler mais atentamente o documento da atividade, percebi que as ações do código deveriam estar em funções separadas

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 #Código será rodado como script no prompt de comando
 #Sendo o primeiro argumento o nome do script (main.py) e o segundo o nome da instância
 import sys
-nome_arquivo = sys.argv[1]
-
-#Carrega a matriz presente na instância e aloca ela em matriz
 import numpy as np
-matriz = np.loadtxt(nome_arquivo, dtype = int)
+
+def le_instancia(instancia):
+    matriz = np.loadtxt(instancia, dtype=int)
+    return matriz
 
 #Pega as dimensões da matriz
-dimensoes = np.shape(matriz)
-
-#Cria o nome do arquivo (nome da instancia e as dimensoes da matriz)
-resultado = nome_arquivo + " " + str(dimensoes[0]) + " " + str(dimensoes[1])
-
+def pega_dimensoes(matriz):
+    dimensoes = np.shape(matriz)
+    return dimensoes
 
 #Imprime a matriz e suas dimensões em um novo arquivo com o nome da instancia e as dimensoes da matriz
-with open(resultado, "w") as arquivo:
-    arquivo.write(f"""A matriz presente no arquivo {nome_arquivo}
+def salva_resultado(instancia, matriz, dimensoes):
+    #Cria o nome do arquivo (nome da instancia e as dimensoes da matriz)
+    resultado = nome_arquivo + " " + str(dimensoes[0]) + " " + str(dimensoes[1])
+
+    with open(resultado, "w") as arquivo:
+        arquivo.write(f"""A matriz presente no arquivo {nome_arquivo}
 {str(matriz)}
 tem dimensoes {dimensoes}""")
+
+#Carrega a matriz presente na instância e aloca ela em matriz
+if __name__=="__main__":
+    nome_arquivo = sys.argv[1]
+
+    #Carrega a matriz presente na instância e aloca ela em matriz
+    matriz = le_instancia(nome_arquivo)
+
+    dimensoes = pega_dimensoes(matriz)
+
+    salva_resultado(nome_arquivo, matriz, dimensoes)
+
+
+
+
+
 
 
 
